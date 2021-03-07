7fe764116f129673ab64d02e7d7365bba43f42bf
Yann Sartori <yannsartori@gmail.com>
2021-03-07 12:19:47 -0500

    Underline ImplicitParameterNode

--- src/ca/mcgill/cs/jetuml/viewers/nodes/ImplicitParameterNodeViewer.java
+++ src/ca/mcgill/cs/jetuml/viewers/nodes/ImplicitParameterNodeViewer.java
@@ -50,8 +50,8 @@ public final class ImplicitParameterNodeViewer extends AbstractNodeViewer
 	private static final int TAIL_HEIGHT = 20; // Piece of the life line below the last call node
 	private static final int TOP_HEIGHT = 60;
 	private static final int Y_GAP_SMALL = 20; 
-	private static final StringViewer NAME_VIEWER = StringViewer.get(StringViewer.VerticalAlign.CENTER, 
-			StringViewer.HorizontalAlign.CENTER, EnumSet.of(StringViewer.FontDecorations.PADDING));
+	private static final StringViewer NAME_VIEWER = StringViewer.get(StringViewer.VerticalAlign.CENTER, StringViewer.HorizontalAlign.CENTER, 
+			EnumSet.of(StringViewer.FontDecorations.UNDERLINED, StringViewer.FontDecorations.PADDING));
 	private static final CallNodeViewer CALL_NODE_VIEWER = new CallNodeViewer();
 	
 	@Override
